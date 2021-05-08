b63d69ef4928aafab4f9378372b7cd6e33d9f57e
byoksul <byoksuul.35@gmail.com>
2021-05-08 06:54:41 +0300

    Proje Geliştirildi.

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -14,7 +14,7 @@ namespace BusinessLayer.ValidationRules
         {
             RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adı Boş Olamaz");
             RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Kısmı Boş Olamaz");
-            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Girişiyapın");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Girişi Yapın");
             RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden Fazla Değer Girişi Yapmayın");
 
         }
